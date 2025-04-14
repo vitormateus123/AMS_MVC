2149c5347123de662ace96b00f461b876f5eb0ea
Vitormateus123 <vitmateus08@gmail.com>
2025-04-14 14:43:04 -0300

    Atualizações

---
+++ Context/ApplicationDataContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMST5_Task11_Interface01.Context
+{
+    public class ApplicationDataContext
+    {
+        
+    }
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -5,11 +5,22 @@ using System.Threading.Tasks;
 
 namespace AMST5_Task11_Interface01.Models
 {
+
     public class Category
     {
-        public Guid Id { get; set; }
-        public string Description { get; set; }
-        public bool IsActive { get; set; }
-        public DateTime CreatedOn { get; set; }
+        public Guid Id { get; private set; }
+        public string Description { get; private set; }
+        public string ImgUrl { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime CreatedOn { get; private set; }
+
+        public Category(string description, string imgUrl)
+        {
+            Id = Guid.NewGuid();
+            Description = description;
+            ImgUrl = imgUrl;
+            IsActive = true;
+            CreatedOn = DateTime.Now;
+        }
     }
 }
